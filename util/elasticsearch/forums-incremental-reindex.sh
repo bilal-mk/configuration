c62007dd8d52c16d21aae9795b26b25f011a6be2
Max Rothman <max@edx.org>
2017-03-06 17:14:47 -0500

    squashy squash

--- util/elasticsearch/forums-incremental-reindex.sh
+++ util/elasticsearch/forums-incremental-reindex.sh
@@ -25,7 +25,7 @@ set -euo pipefail
 INDEX="$1"
 WINDOW="${2:-5}"
 SLEEP_TIME="${3:-60}"
-BATCH_SIZE="${3:-500}"
+BATCH_SIZE="${4:-500}"
 
 if [ "$SLEEP_TIME" -ge "$((WINDOW * 60))" ]; then
   echo 'ERROR: SLEEP_TIME must not be longer than WINDOW, or else documents may be missed.'
